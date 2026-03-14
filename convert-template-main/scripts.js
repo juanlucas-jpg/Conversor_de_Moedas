fb9602ccba81b49295b5a22856a086e93213da90
Seu Nome <juanlcn123@gmail.com>
2026-03-14 16:37:09 -0300

    Configurando o imput amount para receber somente numeros

--- convert-template-main/scripts.js
+++ convert-template-main/scripts.js
@@ -1,1 +1,7 @@
-console.log("Script Adicionado");
+const amount = document.getElementById("amount");
+
+// Configurando o imput amount para receber somente numeros
+amount.addEventListener("input", () => {
+  const hasCharactersRegex = /\D+/g
+  amount.value = amount.value.replace(hasCharactersRegex, "")
+})
